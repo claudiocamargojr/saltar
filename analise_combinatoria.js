4e0c4b85490a76406abd4102f4404292624f7c26
root <root@localhost.localdomain>
2015-06-03 13:16:00 -0300

    Removing trash

--- analise_combinatoria.js
+++
@@ -1,9 +0,0 @@
-var result = db.analysis.aggregate(
-        [
-                {$unwind : "$tags"} ,
-                {$group: { _id : "$tags",count:{$sum:1}}},
-                {$sort:{count:-1}}
-        ]
-)
-
-printjson(result);
--- count_class.js
+++
@@ -1,9 +0,0 @@
-var result = db.analysis.aggregate(
-        [
-                {$unwind : "$tags"} ,
-                {$group: { _id : null, ts: {$addToSet: "$tags"}, count:{$sum:1}}} ,
-                {$project: {_id : 0, tags: "$ts"}}
-        ]
-)
-
-printjson(result);
